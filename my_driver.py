2dead518be41628be8de53a0673b7041e6c5b7f9
LaurelJoannes <31597139+LaurelJoannes@users.noreply.github.com>
2017-11-17 11:47:55 +0100

    Add files via upload

--- my_driver.py
+++ my_driver.py
@@ -1,7 +1,6 @@
-from pytocl.driver import Driver
-from pytocl.car import State, Command
+#from pytocl.driver import Driver
+#from pytocl.car import State, Command
 from neuralnet1 import *
-
 
 NN1 = joblib.load("NN1.pkl")
 prediction = NN1.predict(hoi)
@@ -9,10 +8,10 @@ prediction = pd.DataFrame(prediction)
 prediction.columns = ['ACCELERATION','BRAKE','STEERING']
 print(prediction*sdy+meany)
 
-class MyDriver(Driver):
+#class MyDriver(Driver):
     # Override the `drive` method to create your own driver
     # def drive(self, carstate: State) -> Command:
-    ...
+#    ...
     #     # Interesting stuff
     #     command = Command(...)
     #     return command
