a89819e252103f971439c0a7450ec9959adc6dd8
dlotten68 <dlotten68@gmail.com>
2017-11-21 12:08:30 +0100

    Add files via upload

--- my_driver.py
+++ my_driver.py
@@ -1,17 +1,53 @@
-#from pytocl.driver import Driver
-#from pytocl.car import State, Command
-from neuralnet1 import *
+from pytocl.driver import Driver
+from pytocl.car import State, Command
+import sklearn
+import pandas as pd
+import numpy as np
+from sklearn.externals import joblib
+from sklearn.neural_network import MLPRegressor
 
-NN1 = joblib.load("NN1.pkl")
-prediction = NN1.predict(hoi)
-prediction = pd.DataFrame(prediction)
-prediction.columns = ['ACCELERATION','BRAKE','STEERING']
-print(prediction*sdy+meany)
-
+#from neuralnet1 import *
+# prediction = model.predict(hoi)
+# prediction = pd.DataFrame(prediction)
+# prediction.columns = ['ACCELERATION','BRAKE','STEERING']
+# prediction*sdy+meany
 #class MyDriver(Driver):
     # Override the `drive` method to create your own driver
+    #...
     # def drive(self, carstate: State) -> Command:
-#    ...
     #     # Interesting stuff
     #     command = Command(...)
     #     return command
+
+NN1 = joblib.load("NN1.pkl")
+
+def drive(self, carstate: State) -> Command:
+    """
+    Produces driving command in response to newly received car state.
+
+    This is a dummy driving routine, very dumb and not really considering a
+    lot of inputs. But it will get the car (if not disturbed by other
+    drivers) successfully driven along the race track.
+    """
+    command = Command()
+    speed = (carstate.speed_x**2+carstate.speed_y**2+carstate.speed_z**2)**.5
+    command.steering = self.steering_ctrl.control(
+        NN1.predict([speed,
+                     carstate.distance_from_center,
+                     carstate.angle,
+                     carstate.distances_from_edge].values.reshape(1,-1)),
+        carstate.current_lap_time
+    )
+
+    # ACC_LATERAL_MAX = 6400 * 5
+    # v_x = min(80, math.sqrt(ACC_LATERAL_MAX / abs(command.steering)))
+    v_x = 80
+
+    self.accelerate(carstate, v_x, command)
+
+    if self.data_logger:
+        self.data_logger.log(carstate, command)
+
+    return command
+
+
