c9ef641f77b5648a78eeea02066f128a6d692634
LaurelJoannes <31597139+LaurelJoannes@users.noreply.github.com>
2017-11-24 13:49:32 +0100

    Add files via upload

--- neuralnet1.py
+++ neuralnet1.py
@@ -3,11 +3,12 @@ import pandas as pd
 import numpy as np
 import os
 import pickle
+import random
 from sklearn.neural_network import MLPRegressor
 from sklearn.externals import joblib
 print(os.getcwd())
-X = pd.read_csv('aalborg.csv', index_col= None, skipfooter = 1, engine = 'python') # laatste kolomnaam blanco (edge18?) --> onbekend
-#print(X)
+X = pd.read_csv('train2.csv', index_col= None, engine = 'python') # laatste kolomnaam blanco (edge18?) --> onbekend
+print(X)
 X.describe()
 
 y = X[['ACCELERATION','BRAKE','STEERING']]
@@ -22,13 +23,16 @@ scaledX = (X - meanX)/sdX
 
 scaledy = (y - meany)/sdy
 
-model = MLPRegressor(hidden_layer_sizes=(30,30,30))
+model = MLPRegressor(hidden_layer_sizes=(12,12))
 model.fit(scaledX, scaledy)
+
 hoi = scaledX.iloc[4].values.reshape(1,-1)
+print(type(hoi))
+print((hoi.shape))
 prediction = model.predict(hoi)
 prediction = pd.DataFrame(prediction)
 prediction.columns = ['ACCELERATION','BRAKE','STEERING']
-prediction*sdy+meany
+print(prediction*sdy+meany)
 
-#NN1 = pickle.dumps(model)
+
 joblib.dump(model, 'NN1.pkl')
