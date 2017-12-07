0861f37db2746ba89feb33dbe84e8bf3707769ab
jkn354 <33344558+jkn354@users.noreply.github.com>
2017-12-07 12:10:52 +0100

    Add files via upload

--- creatingTrainData.py
+++ creatingTrainData.py
@@ -6,7 +6,7 @@ import pickle
 aalborg = pd.read_csv('aalborg.csv', index_col=None, engine='python')
 alpine1 = pd.read_csv('alpine-1.csv', index_col=None, engine='python')
 fspeedway = pd.read_csv('f-speedway.csv', index_col=None, engine='python')
-testdrive = pd.read_csv('testdrive.csv', index_col=0, engine='python')
+#testdrive = pd.read_csv('testdrive.csv', index_col=0, engine='python')
 
 #  Drop BRAKE, no brake in testdrive
 aalborg.drop(['BRAKE'], axis=1, inplace=True)
@@ -14,21 +14,23 @@ alpine1.drop(['BRAKE'], axis=1, inplace=True)
 fspeedway.drop(['BRAKE'], axis=1, inplace=True)
 
 #  Calculate speed with euclidean distance
-testdrive['SPEED'] = (testdrive['speedX'] ** 2 + testdrive['speedY'] ** 2) ** .5
-#  Drop columns not in aalborg, alpine1, fspeedway
-testdrive.drop(['speedX', 'speedY', 'speedZ', 'rpm', 'gear', 'setGear'], axis=1, inplace=True)
-#  Rearrange columns of testdrive
-testdrive = testdrive[
-    ['setAcceleration', 'setSteer', 'SPEED', 'trackPos', 'angle', 'track0', 'track1', 'track2', 'track3', 'track4',
-     'track5', 'track6', 'track7', 'track8', 'track9', 'track10', 'track11', 'track12', 'track13', 'track14', 'track15',
-     'track16', 'track17', 'track18']]
+# testdrive['SPEED'] = (testdrive['speedX'] ** 2 + testdrive['speedY'] ** 2) ** .5
+# #  Drop columns not in aalborg, alpine1, fspeedway
+# testdrive.drop(['speedX', 'speedY', 'speedZ', 'rpm', 'gear', 'setGear'], axis=1, inplace=True)
+# #  Rearrange columns of testdrive
+# testdrive = testdrive[
+#     ['setAcceleration', 'setSteer', 'SPEED', 'trackPos', 'angle', 'track0', 'track1', 'track2', 'track3', 'track4',
+#      'track5', 'track6', 'track7', 'track8', 'track9', 'track10', 'track11', 'track12', 'track13', 'track14', 'track15',
+#      'track16', 'track17', 'track18']]
 #  Set new names testdrive, so they can be concate
 columnNames = ['ACCELERATION', 'STEERING', 'SPEED', 'TRACK_POSITION', 'ANGLE_TO_TRACK_AXIS', 'TRACK_EDGE_0',
                'TRACK_EDGE_1', 'TRACK_EDGE_2', 'TRACK_EDGE_3', 'TRACK_EDGE_4', 'TRACK_EDGE_5', 'TRACK_EDGE_6',
                'TRACK_EDGE_7', 'TRACK_EDGE_8', 'TRACK_EDGE_9', 'TRACK_EDGE_10', 'TRACK_EDGE_11', 'TRACK_EDGE_12',
                'TRACK_EDGE_13', 'TRACK_EDGE_14', 'TRACK_EDGE_15', 'TRACK_EDGE_16', 'TRACK_EDGE_17', 'TRACK_EDGE_18']
 #  Concatenate dataframes to one super train data frame
-trainDataFrame = pd.DataFrame(np.concatenate((aalborg.values, alpine1.values, fspeedway.values, testdrive.values)),
+#trainDataFrame = pd.DataFrame(np.concatenate((aalborg.values, alpine1.values, fspeedway.values, testdrive.values)),
+#                              columns=columnNames)
+trainDataFrame = pd.DataFrame(np.concatenate((aalborg.values, alpine1.values, fspeedway.values)),
                               columns=columnNames)
 # trainDataFrame = pd.concat([aalborg, alpine1, fspeedway, testdrive], ignore_index=True)
 #  Make a list of tracknames
@@ -73,9 +75,11 @@ def cornerLearner(trackData):
 
 trainDataFrame['CORNER'] = trainDataFrame[trackNAMES].apply(func=cornerLearner, axis=1);
 trainDataFrame['TRACK_WIDTH'] = trainDataFrame['TRACK_EDGE_0'] + trainDataFrame['TRACK_EDGE_18']
-trainDataFrame = trainDataFrame[['ACCELERATION', 'STEERING', 'TRACK_POSITION', 'ANGLE_TO_TRACK_AXIS', 'TRACK_EDGE_0',
-                                 'TRACK_EDGE_7', 'TRACK_EDGE_9', 'TRACK_EDGE_11', 'TRACK_EDGE_18', 'MAX_DISTANCE',
-                                 'CORNER', 'TRACK_WIDTH', 'SPEED']]
+#trainDataFrame = trainDataFrame[['ACCELERATION', 'STEERING', 'TRACK_POSITION', 'ANGLE_TO_TRACK_AXIS', 'TRACK_EDGE_0',
+#                                 'TRACK_EDGE_7', 'TRACK_EDGE_9', 'TRACK_EDGE_11', 'TRACK_EDGE_18', 'MAX_DISTANCE',
+#                                 'CORNER', 'TRACK_WIDTH', 'SPEED']]
+trainDataFrame = trainDataFrame[['STEERING', 'TRACK_POSITION', 'ANGLE_TO_TRACK_AXIS', 'MAX_DISTANCE', 'CORNER',
+                                 'TRACK_WIDTH', 'SPEED']]
 
 scalingFactor = trainDataFrame.drop(['SPEED'], axis=1).abs().max(axis=0)
 speedFactor = trainDataFrame['SPEED'].abs().max()
